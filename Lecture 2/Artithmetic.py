104bf2607d2822258c98d8c75025735234117c96
s6906022510075-bit <s6906022510075@email.kmutnb.ac.th>
2026-07-07 15:10:06 +0700

    Lecture02

---
+++ Lecture 2/Artithmetic.py
@@ -0,0 +1,29 @@
+x =15
+y = 4
+
+#Output: x+y=19
+print('x+y =',x+y)
+
+#output: x-y=11
+print('x-y = ',x-y)
+
+#Output: x*y = 60
+print('x*y = ',x*y)
+
+#Output: x / y = 3.75
+print('x/y = ',x/y)
+
+#Output: x % y = 3
+print('x%y = ',x%y) 
+
+#Output: x//y = 3
+print('x//y = ',x//y)
+
+#Output: x**y = 50625
+print('x**y = ',x**y)
+
+#output: 5+18//2-25%4=13 ทำ %,// ก่อนแล้วค่อย +,-
+print(5+18//2-25%4)
+
+#output: (5+18)//2-25%4=10 ()มีสิทธิสูงกว่า
+print((5+18)//2-25%4)#yyyyyy
---
+++ Lecture 2/Precedence.py
@@ -0,0 +1,14 @@
+#first_name = input("Enter your first name: ")
+#Last_name = input("Enter your last name: ")
+#print("Hello," + first_name + " " +Last_name + "!Welcome to the program.")
+
+#Get the user's name, age, and income
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+income = float(input("What is your income?: "))
+
+#Display the data
+print('Here is the data you entered: ')
+print('Name: ',name)
+print('Age: ',age)#yyyyyyy
+print('Income:', format(income,'12,.2f'))
