36341d98924564b36832cbd8336e21466352e623
s6906022510075-bit <s6906022510075@email.kmutnb.ac.th>
2026-07-07 15:25:15 +0700

    Lecture02

---
+++ Lecture 2/Example1.py
@@ -0,0 +1,10 @@
+#Getting user input and converting to int
+age = input('Enter your age: ')
+age = int(age)
+
+#Getting user input and converting to float
+height = input('Enter your height: ')
+height = float(height)
+
+print('Your age is', str(age), 'years old and your height is', str(height), 'feets tall.')
+
---
+++ Lecture 2/Example3.py
@@ -0,0 +1,3 @@
+temp = float(input('Enter the temperature in Celsius: '))
+fahrenheit = (temp * 9/5) + 32       
+print('The temperature in Fahrenheit is', format(fahrenheit, ".2f"))
